cd911cac24ddf637c604c5a1bfe0eb2d84235119
JasmineIslamNY <jasmine@tek-genie.com>
2016-03-08 22:02:50 -0500

    20160308_2202
    4.2.1

--- Mapper/app/src/main/java/com/tek_genie/mapper/MapsActivity.java
+++ Mapper/app/src/main/java/com/tek_genie/mapper/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.tek_genie.mapper;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,11 +15,39 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.logging.Handler;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.*;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,ConnectionCallbacks, OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+                showLocation(mCurrentLocation);
+    }
+    protected void showLocation(Location mCurrentLocation) {
+        if (mCurrentLocation != null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), 15));
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +57,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
 
@@ -41,7 +77,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
 
+
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(19), 2000, null);
+            //.moveCamera(CameraUpdateFactory.newLatLngZoom(mMap.setMyLocationEnabled(true), 10));
+        /*
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //LatLng home = new LatLng(40.666954, -73.715123);
@@ -57,7 +98,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 ;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(thinkful, 10));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(19), 2000, null);
-        /**
+        *//**
         Handler handler;
         handler = new Handler();
         handler.postDelayed(new Runnable() {
