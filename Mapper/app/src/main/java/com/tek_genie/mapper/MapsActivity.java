09653d6f22c58e72c7cd120f4a81fc1fd1dd4630
JasmineIslamNY <jasmine@tek-genie.com>
2016-03-08 20:21:37 -0500

    20160308_2021
    4.1.4

--- Mapper/app/src/main/java/com/tek_genie/mapper/MapsActivity.java
+++ Mapper/app/src/main/java/com/tek_genie/mapper/MapsActivity.java
@@ -3,12 +3,16 @@ package com.tek_genie.mapper;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.logging.Handler;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,9 +43,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        //LatLng sydney = new LatLng(-34, 151);
         //LatLng home = new LatLng(40.666954, -73.715123);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker at Home"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //mMap.addMarker(new MarkerOptions().position(home).title("Marker at Home"));
+        LatLng thinkful = new LatLng(40.72493, -73.996599);
+        mMap.addMarker(new MarkerOptions()
+                .position(thinkful)
+                .title("Thinkful Headquarters")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.thinkful))
+                .snippet("On a mission to reinvent education"))
+                .showInfoWindow()
+                ;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(thinkful, 10));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(19), 2000, null);
+        /**
+        Handler handler;
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(19), 2000, null);
+            }
+        }, 2000);
+         */
+
     }
 }
