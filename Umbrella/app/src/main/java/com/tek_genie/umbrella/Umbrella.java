97b4c19ddd1284a2d96ba359a08c83e2e12fc157
JasmineIslamNY <jasmine@tek-genie.com>
2016-03-16 01:05:17 -0400

    20160316_104
    Working on 4.3

--- Umbrella/app/src/main/java/com/tek_genie/umbrella/Umbrella.java
+++ Umbrella/app/src/main/java/com/tek_genie/umbrella/Umbrella.java
@@ -24,11 +24,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Umbrella extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    private GoogleMap mMap;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mCurrentLocation;
+    protected GoogleMap mMap;
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mCurrentLocation;
     double latitude;
     double longitude;
 
@@ -36,6 +46,7 @@ public class Umbrella extends AppCompatActivity implements OnMapReadyCallback, G
     protected void onStart() {
         super.onStart();
         mGoogleApiClient.connect();
+        onLocationChanged(mCurrentLocation);
     }
     @Override
     public void onConnected(Bundle connectionHint) {
@@ -47,15 +58,142 @@ public class Umbrella extends AppCompatActivity implements OnMapReadyCallback, G
     protected void updateLocation() {
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
+    protected String doINeedAnUmbrella(String location){
+        String urlStringStart = "http://api.wunderground.com/api/5a0befd8a35cbb56/conditions/q/";
+        String urlStringEnd = ".json";
+        String urlStringLocation = location;
+        String urlString = urlStringStart + urlStringLocation + urlStringEnd;
+        InputStream umbrellaJSON;
+
+        umbrellaJSON = getJSONResult(urlString); //this will return an InputStream
+        String umbrellaYesNo = getResultFromJSON(umbrellaJSON, "umbrella"); //this will return "Yes" if there is rain or snow
+        return umbrellaYesNo;
+    }
+
+    protected String determineCityState (double latitude, double longitude) {
+        String urlStringStart = "http://api.wunderground.com/api/5a0befd8a35cbb56/geolookup/q/";
+        String urlStringMiddle = ",";
+        String urlStringEnd = ".json";
+        String latitudeString = String.valueOf(latitude);
+        String longitudeString = String.valueOf(longitude);
+        String urlString = urlStringStart + latitudeString + urlStringMiddle + longitudeString + urlStringEnd;
+        InputStream cityStateJSON;
+
+        cityStateJSON = getJSONResult(urlString); //this will return an InputStream
+        String cityAndState = getResultFromJSON(cityStateJSON, "cityState"); //this will return State/City as a string, i.e. CA/San Francisco
+        return cityAndState;
+    }
+
+    protected String getResultFromJSON (InputStream inputJSON, String type) {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(inputJSON));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+            if (type == "cityState") {
+            String returnedCity = getJSONKeyPairValue(stringBuilder, "city");
+            String returnedState = getJSONKeyPairValue(stringBuilder, "state");
+            String cityAndState = returnedCity + "/" + returnedState;
+            return cityAndState;
+            }
+            else {
+                String umbrella = getJSONKeyPairValue(stringBuilder, "weather");
+                return umbrella;
+            }
+
+            //Log.i("Returned data", stringBuilder.toString());
+        } catch (Exception e) {
+            Log.e("MainActivity", "Error", e);
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+        return "Error!";
+    }
+
+    protected String getJSONKeyPairValue (StringBuilder stringBuilder, String returnKey) {
+        if (returnKey == "city" || returnKey == "state"){
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(stringBuilder.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONObject location = null;
+            try {
+                location = jsonObject.getJSONObject("location");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String cityState = null;
+            try {
+                cityState = location.getString(returnKey);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return cityState;
+        }
+        else {
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(stringBuilder.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (jsonObject.has("rain") || jsonObject.has("snow")) {
+                return("Yes");
+            } else {
+                return("No");
+            }
+        }
+    }
+
+    protected InputStream getJSONResult (String urlString){
+        HttpURLConnection urlConnection = null;
+        InputStream jsonFile = null;
+
+        try {
+            URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            jsonFile = urlConnection.getInputStream();
+            return jsonFile;
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error ", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+        return jsonFile;
+    }
     @Override
     public void onLocationChanged(Location location) {
         //showLocation(location);
         mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
-        Log.i("Where am I?", "Latitude: " + mCurrentLocation.getLatitude() + ", Longitude:" + mCurrentLocation.getLongitude());
+        if (mCurrentLocation != null) {
+            latitude = mCurrentLocation.getLatitude();
+            longitude = mCurrentLocation.getLongitude();
+        }
+        else {
+            latitude = 40.666954;
+            longitude = -73.715123;
+        }
+        Log.i("Where am I?", "Latitude: " + latitude + ", Longitude:" + longitude);
+        String cityAndState = determineCityState(latitude, longitude);
+        String umbrellaToday = doINeedAnUmbrella(cityAndState);
+        Log.i("Do I need an umbrella today", "? " + umbrellaToday);
     }
 
     @Override
@@ -65,7 +203,7 @@ public class Umbrella extends AppCompatActivity implements OnMapReadyCallback, G
     public void onConnectionFailed(ConnectionResult connectionResult) {
     }
 
-    /*
+
     @Override
     public void onProviderDisabled(String s) {
 
@@ -81,7 +219,7 @@ public class Umbrella extends AppCompatActivity implements OnMapReadyCallback, G
 
     }
 
-    */
+
     @Override
     protected void onPause() {
         super.onPause();
