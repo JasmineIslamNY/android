dd9fee211badb7d9c0ae9c37249d9e0bf1284dd7
JasmineIslamNY <jasmine@tek-genie.com>
2016-04-20 19:43:00 -0400

    20160420_1942
    5.3.3

---
+++ Notes/app/src/main/java/com/tek_genie/notes/NoteColorActivity.java
@@ -0,0 +1,13 @@
+package com.tek_genie.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteColorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_color);
+    }
+}
