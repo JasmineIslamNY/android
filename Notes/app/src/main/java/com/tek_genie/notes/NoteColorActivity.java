2cd2e969fc108e27bb5ef3e7120c7666aac67c33
JasmineIslamNY <jasmine@tek-genie.com>
2016-04-26 13:32:00 -0400

    20160426_1331
    Working on Notes APP

--- Notes/app/src/main/java/com/tek_genie/notes/NoteColorActivity.java
+++ Notes/app/src/main/java/com/tek_genie/notes/NoteColorActivity.java
@@ -1,13 +1,49 @@
 package com.tek_genie.notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class NoteColorActivity extends AppCompatActivity {
+
+    private String backGroundColor;
+    private String foreGroundColor;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_color);
+
+
+        Intent intent = getIntent();
+        /*
+        note = (NoteListItem) intent.getSerializableExtra("Note");
+        noteEditText = (EditText) findViewById(R.id.note_text_edit);
+        noteEditText.setText(note.getText());
+        */
+
+        saveButton = (Button) findViewById(R.id.buttonColorSelectorSave);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                foreGroundColor = "";
+                backGroundColor = "";
+
+                Intent intent = new Intent();
+                intent.putExtra("BackGroundColor", backGroundColor);
+                intent.putExtra("ForeGroundColor", foreGroundColor);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+        });
+
     }
 }
+
+
