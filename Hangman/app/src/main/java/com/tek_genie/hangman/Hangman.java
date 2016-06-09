bea411bd25bf1deb4730c90e829e92857e16a772
JasmineIslamNY <jasmine@tek-genie.com>
2016-06-08 20:37:14 -0400

    20160608_2036 (Hangman)
    Working clue display, display _ for letters, check for failed and
    successful letters, display successful letters

--- Hangman/app/src/main/java/com/tek_genie/hangman/Hangman.java
+++ Hangman/app/src/main/java/com/tek_genie/hangman/Hangman.java
@@ -25,15 +25,18 @@ public class Hangman extends AppCompatActivity {
     final String TAG = "MyData";
     public Integer totalGames;
     public Integer totalWonGames;
+    private int numberOfFailedTries = 0;
     public TextView tries;
     public TextView letters;
     public TextView countWonTotal;
     public TextView labelLettersTried;
-    public TextView labelNumberOfTries;
-    private String [] name;
+    public TextView labelNumberOfFailedTries;
+    private String[] nameAndInfo;
     private String gmWonTotal;
-
-
+    private String secretName;
+    private StringBuilder gameName;
+    private TextView gameNameLabel;
+    private TextView gameClue;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +44,24 @@ public class Hangman extends AppCompatActivity {
 
         setContentView(R.layout.activity_hangman);
         Intent intent = getIntent();
-        name = intent.getStringArrayExtra("nameIntentExtra");
+        nameAndInfo = intent.getStringArrayExtra("nameIntentExtra");
         gmWonTotal = intent.getStringExtra("gamesWonTotalIntentExtra");
-        Log.i(TAG, name[0] +" "+ name[1]);
-        Log.i(TAG, gmWonTotal);
+        Log.i(TAG, "nameAndInfo 0 and 1: " + nameAndInfo[0] + " " + nameAndInfo[1]);
+        Log.i(TAG, "gmWonTotal: " + gmWonTotal);
+        secretName = nameAndInfo[0] + " " + nameAndInfo[1];
+        Log.i(TAG, "secretName = " + secretName);
 
-        tries = (TextView) findViewById(R.id.labelNumberOfTries);
-        tries.setText("0 Tries");
+        gameName = new StringBuilder(underscoreName());
+        updateGameNameDisplay();
+        Log.i(TAG, "gameName in onCreate: " + gameName);
+        updateNumberOfFailedTries();
+
         letters = (TextView) findViewById(R.id.labelLettersTried);
         letters.setText("No Letters Tried Yet");
         countWonTotal = (TextView) findViewById(R.id.countWonTotal);
         countWonTotal.setText(gmWonTotal);
+        gameClue = (TextView) findViewById(R.id.labelClue);
+        gameClue.setText(nameAndInfo[6]);
 
         submitLetter = (Button) findViewById(R.id.buttonSubmitLetter);
         submitLetter.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +69,12 @@ public class Hangman extends AppCompatActivity {
             public void onClick(View v) {
                 EditText nextLetter = (EditText) findViewById(R.id.edittextEnterNextLetter);
                 Log.i(TAG, "Entered: " + nextLetter.getText().toString());
+                
+                int isLetterFound = checkNameForLetter(nextLetter.getText().toString());
+                if (isLetterFound == 0){
+                    numberOfFailedTries += 1;
+                    updateNumberOfFailedTries();
+                }
 
                 updateLettersTried(nextLetter.getText().toString());
                 nextLetter.setText("");
@@ -82,13 +98,48 @@ public class Hangman extends AppCompatActivity {
         });
     }
 
-    private void updateLettersTried (String nextLetter) {
+    private void updateLettersTried(String nextLetter) {
         labelLettersTried = (TextView) findViewById(R.id.labelLettersTried);
         if (labelLettersTried.getText().toString() == "" || labelLettersTried.getText().toString() == "No Letters Tried Yet") {
             labelLettersTried.setText(nextLetter);
-        }
-        else{
+        } else {
             labelLettersTried.setText(labelLettersTried.getText().toString() + ", " + nextLetter);
         }
+    }
+
+    private String underscoreName() {
+        String name = "";
+        for (int i = 0; i < secretName.length(); i++) {
+            if (secretName.substring(i, i+1).equals(" ")) {
+                name = name + " ";
+            } else {
+                name = name + "_";
+            }
+        }
+        return name;
+    }
+
+    private int checkNameForLetter(String letter) {
+        int foundLetter = 0;
+        for (int i = 0; i < secretName.length(); i++) {
+            if (letter.toUpperCase().equals(secretName.substring(i, i+1).toUpperCase())) {
+                foundLetter = 1;
+                String letterToUpper = letter.toUpperCase();
+                gameName.setCharAt(i, letterToUpper.charAt(0));
+            }
+        }
+        updateGameNameDisplay();
+
+        return foundLetter;
+    }
+
+    private void updateGameNameDisplay () {
+        gameNameLabel = (TextView) findViewById(R.id.labelGameName);
+        gameNameLabel.setText(gameName);
+    }
+
+    private void updateNumberOfFailedTries () {
+        tries = (TextView) findViewById(R.id.labelNumberOfFailedTries);
+        tries.setText(numberOfFailedTries + "Failed Tries");
     }
 }
